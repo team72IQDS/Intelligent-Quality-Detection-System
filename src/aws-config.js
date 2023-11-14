feedd6abdabc0240770e70b0b92668dcecd4eadb
Parth Amrute <parthamrute@Parths-MacBook-Pro-2.local>
2023-11-13 21:38:05 -0600

    changing env variables

--- src/aws-config.js
+++ src/aws-config.js
@@ -2,8 +2,8 @@ import AWS from "aws-sdk";
 
 AWS.config.update({
   region: "us-east-2", // Replace with your AWS region
-  accessKeyId: process.env.AWS_ACCESS_KEY,
-  secretAccessKey: process.env.AWS_SECRET_KEY,
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_KEY_ID,
 });
 
 export default AWS;
